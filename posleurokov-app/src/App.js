f50f85d96515fc80b0c12eea804813b7699392d6
Maria Zaitsava <mzaitsava@gomel.iba.by>
2022-07-19 15:03:50 +0300

    added font import

--- posleurokov-app/src/App.js
+++ posleurokov-app/src/App.js
@@ -4,15 +4,14 @@ import { Button } from './components/shared'
 function App() {
     return (
         <Button
-            onClick={() => {
-                alert(1)
-            }}
             height='50px'
-            width='120px'
             background='#EA2182'
             borderRadius='10px'
             color='#FFFFFF'
-        ><p>Content</p></Button>
+            onClick={() => {
+                alert(1)
+            }}
+        >Добавить занятие</Button>
     );
 }
 
--- posleurokov-app/src/components/shared/Button.js
+++ posleurokov-app/src/components/shared/Button.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import './Button.module.css';
+import styles from './Button.module.css';
 
 function Button({type = 'button', children, onClick, ...rest}) {
     return (
         <button
+            className={styles.button}
             type={type}
             onClick={onClick}
             style={{...rest}}
