9eb970756a45601a7d3ebe571b51684e4942ef81
Maria Zaitsava <mzaitsava@gomel.iba.by>
2022-07-28 11:20:41 +0300

    add responsive for tabsbar

--- posleurokov-app/src/components/shared/TabsBar/TabsBar.jsx
+++ posleurokov-app/src/components/shared/TabsBar/TabsBar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useState } from 'react';
 import styles from './TabsBar.module.scss';
 import logo from 'assets/img/map.png';
+import other from 'assets/img/other.png';
 
 import { TabBarNav, TabBarItem } from '../';
 
@@ -12,20 +13,55 @@ const TabsBar = ({ items, ...rest }) => {
     setActiveTab(index);
   };
 
+  const showAll = () => {
+    const tabs = document.getElementsByName('showAll');
+    for(let i = 0; i < tabs.length; i++) {
+      if (tabs[i].style.display === 'inline') {
+        tabs[i].style.display = 'none';
+      } else {
+        tabs[i].style.display = 'inline';
+      }
+    }
+  };
+
   return (
     <div
       style={{ ...rest }}
     >
       <div className={styles.tabnav}>
-        {items.map((page, i) => (
-          <TabBarNav
-            key={page.title}
-            number={page.count}
-            logo={logo}
-            isActive={i === activeTab}
-            onClick={() => openTab(i)}
-          >{page.title}</TabBarNav>
-        ))}
+        {window.screen.width > 321 ? (
+          items.map((page, i) => (
+            <TabBarNav
+              key={page.title}
+              number={page.count}
+              logo={logo}
+              isActive={i === activeTab}
+              onClick={() => openTab(i)}
+            >{page.title}</TabBarNav>
+          ))
+        ) : (
+          items.map((page, i) => (
+            i < 3 ? (
+              <TabBarNav
+                key={page.title}
+                number={page.count}
+                logo={logo}
+                isActive={i === activeTab}
+                onClick={() => openTab(i)}
+              >{page.title}</TabBarNav>
+            ) : (
+              <div key={page.title} name='showAll' style={{ display: 'none'}}>
+                <TabBarNav
+                  number={page.count}
+                  logo={logo}
+                  isActive={i === activeTab}
+                  onClick={() => openTab(i)}
+                >{page.title}</TabBarNav>
+              </div>
+            )
+          ))
+        )}
+        <button onClick={() => showAll()} className={styles.hide}><img src={other} alt='other'/></button>
       </div>
       <div>
         {items[activeTab] && <TabBarItem { ...items[activeTab] } padding='28px'/>}
--- posleurokov-app/src/components/test/components-demo/ComponentsDemo.jsx
+++ posleurokov-app/src/components/test/components-demo/ComponentsDemo.jsx
@@ -16,6 +16,7 @@ const ComponentsDemo = () => {
   const items = [
     { title: 'London', count: '100', content: 'London is the capital city of England.' },
     { title: 'Paris', count: '10', content: 'Paris is the capital of France.' },
+    { title: 'Berlin', count: '10', content: 'Berlin is the capital of Germany.' },
     { title: 'На карте', count: '-1', content: 'Tokyo is the capital of Japan.' },
   ];
   return (
