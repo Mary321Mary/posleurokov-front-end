2bde6bcafe088020a738061b83cdcb312d1c7a62
Maria Zaitsava <mzaitsava@gomel.iba.by>
2022-07-29 13:20:19 +0300

    add responsive for header

--- posleurokov-app/src/components/Header/Header.jsx
+++ posleurokov-app/src/components/Header/Header.jsx
@@ -1,7 +1,18 @@
 import styles from './Header.module.scss';
 import { Logo, Button, Link } from 'components';
+import { Sheet } from 'components/shared'
+import tel from 'assets/img/tel.png'
+import menu from 'assets/img/menu.png'
 
 const Header = () => {
+  const openNav = () => {
+    document.getElementById('menu').style.width = '250px';
+  }
+
+  const closeNav = () => {
+    document.getElementById('menu').style.width = '0';
+  }
+
   return (
     <>
       <div>
@@ -10,17 +21,35 @@ const Header = () => {
           <option value="gomel">Гомель</option>
         </select>
       </div>
-      <div>
-        <div>
-          <p>Поможем выбрать</p>
-          <p>+375 29 113-67-97</p>
-        </div>
+      <div className={styles.hide}>
+        <Sheet
+          display='flex'
+          alignItems='center'
+          padding='10px'
+        >
+          <img src={tel} alt='Телефон' style={{marginRight:'15.08px'}}/>
+          <div>
+            <div style={{
+              fontFamily: 'Roboto-Medium',
+              fontWeight: '500',
+              fontSize: '14px',
+              lineHeight: '16px',
+              color: '#9E9E9E'
+            }}>Поможем выбрать</div>
+            <div style={{
+              fontFamily: 'Roboto-Medium',
+              fontWeight: '500',
+              fontSize: '16px',
+              lineHeight: '19px',
+              color: '#5F6060'
+            }}>+375 29 113-67-97</div>
+          </div>
+        </Sheet>
         <div className={styles['vertical-line']}></div>
         <div>
           <Link
             path='/'
             fontFamily='Roboto-Bold'
-            fontStyle='normal'
             fontWeight='700'
             fontSize='16px'
             lineHeight='19px'
@@ -33,7 +62,6 @@ const Header = () => {
           <Link
             path='/'
             fontFamily='Roboto-Bold'
-            fontStyle='normal'
             fontWeight='700'
             fontSize='16px'
             lineHeight='19px'
@@ -44,6 +72,58 @@ const Header = () => {
           </Link>
         </div>
         <Button marginLeft="34px">Добавить занятие</Button>
+      </div>
+      <div className={styles.show}>
+        <img src={menu} alt='menu' onClick={() => openNav()} style={{marginRight: '16px'}}/>
+        <div id='menu' className={styles.menu}>
+          <a className={styles.closebtn} onClick={() => closeNav()}>&times;</a>
+          <Sheet
+            display='flex'
+            alignItems='center'
+            padding='10px'
+          >
+            <img src={tel} alt='Телефон' style={{marginRight:'15.08px'}}/>
+            <div>
+              <div style={{
+                fontFamily: 'Roboto-Medium',
+                fontWeight: '500',
+                fontSize: '14px',
+                lineHeight: '16px',
+                color: '#9E9E9E'
+              }}>Поможем выбрать</div>
+              <div style={{
+                fontFamily: 'Roboto-Medium',
+                fontWeight: '500',
+                fontSize: '16px',
+                lineHeight: '19px',
+                color: '#5F6060'
+              }}>+375 29 113-67-97</div>
+            </div>
+          </Sheet>
+          <div>
+            <Link
+              path='/'
+              fontFamily='Roboto-Bold'
+              fontWeight='700'
+              fontSize='16px'
+              lineHeight='19px'
+              color='#5F6060'
+            >
+              Вход
+            </Link>
+            <Link
+              path='/'
+              fontFamily='Roboto-Bold'
+              fontWeight='700'
+              fontSize='16px'
+              lineHeight='19px'
+              color='#5F6060'
+            >
+              Регистрация
+            </Link>
+          </div>
+          <Button marginLeft='34px'>Добавить занятие</Button>
+        </div>
       </div>
     </>
   );
