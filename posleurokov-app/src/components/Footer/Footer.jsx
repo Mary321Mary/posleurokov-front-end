765edf51bec21f44381b4d874b058d596651ea2c
Maria Zaitsava <mzaitsava@gomel.iba.by>
2022-08-03 10:29:48 +0300

    add responsive for footer

--- posleurokov-app/src/components/Footer/Footer.jsx
+++ posleurokov-app/src/components/Footer/Footer.jsx
@@ -24,16 +24,53 @@ const Footer = () => {
     },
   ];
 
+  const navLittleItems = [
+    {
+      path: '#',
+      text: 'Главная',
+    },
+    {
+      path: '#',
+      text: 'О проекте',
+    },
+    {
+      path: '#',
+      text: 'Условия',
+    },
+    {
+      path: '#',
+      text: 'Контакты',
+    },
+    {
+      path: '/qwe/qasd',
+      text: 'Вопросы и ответы',
+    },
+  ];
+
   const nav = navItems.map((item, i) => (
     <li key={item.text} className={styles['nav__item']}>
       <Link path={item.path}>{item.text}</Link>
       {i < navItems.length - 1 && <div className={styles['vertical-line']} />}
     </li>
   ));
+
+  const navLittle = navLittleItems.map((item) => (
+    <li key={item.text} className={styles['nav__item']}>
+      <Link path={item.path}>{item.text}</Link>
+    </li>
+  ));
   return (
     <section className={styles.container}>
       <nav>
-        <ul>{nav}</ul>
+        <ul className={styles.nav}>
+          {
+            window.screen.width > 760 ? (
+              nav
+            ) : (
+              navLittle
+            )
+          }
+        </ul>
       </nav>
       <Button width="210px">ОСТАВИТЬ МЕСТО</Button>
     </section>
