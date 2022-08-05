18b97beb8882ddb041b1cd9c2f745e0a72a87c12
Maria Zaitsava <mzaitsava@gomel.iba.by>
2022-08-05 12:33:15 +0300

    fix style

--- posleurokov-app/src/components/Additional/Additional.jsx
+++ posleurokov-app/src/components/Additional/Additional.jsx
@@ -1,8 +1,5 @@
 import styles from './Additional.module.scss';
 import { useState, useEffect } from 'react';
-import add from 'assets/img/additional.svg';
-import union from 'assets/img/Union.svg';
-import bgr from 'assets/img/bgr.svg';
 import { axiosAPI } from 'plugins/axios';
 
 const Additional = () => {
@@ -19,16 +16,11 @@ const Additional = () => {
 
   return (
     <div className={styles.additional}>
-      <img src={add} alt='Занятие' className={styles.add} />
-      <img src={union} alt='флажок' className={styles.union} />
-      <div>
-        {additional.price !== null ? (
-          <p className={styles.exist}>{additional.price} Р.</p>
-        ) : (
-          <p className={styles.absent}>не указана</p>
-        )}
-        <img src={bgr} alt='темный фон' />
-      </div>
+      {additional.price !== null ? (
+        <p className={styles.exist}>{additional.price} Р.</p>
+      ) : (
+        <p className={styles.absent}>не указана</p>
+      )}
       <p className={styles.name}>{additional.name}</p>
     </div>
   );
