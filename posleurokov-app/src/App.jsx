b4d8fe34c5f8b9a65d2c179a01acd7357d711b2c
Maria Zaitsava <mzaitsava@gomel.iba.by>
2022-08-09 14:25:46 +0300

    add title

--- posleurokov-app/src/App.jsx
+++ posleurokov-app/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.scss';
 import { DefaultLayout } from './layout';
 import { NotFoundPage, Main, Catalogue } from './pages';
+import Helmet from 'react-helmet';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 const App = () => {
   return (
     <Router>
+      <Helmet titleTemplate="Все Кружки :: %s" />
       <DefaultLayout>
         <Routes>
           <Route exact path="/" element={<Main />} />
