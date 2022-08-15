e4a6d3bd8ba23da108deb6da8cbcabc975b08f9a
Maria Zaitsava <mzaitsava@gomel.iba.by>
2022-08-15 10:43:17 +0300

    small fixes

--- posleurokov-app/src/plugins/endpoints.js
+++ posleurokov-app/src/plugins/endpoints.js
@@ -1,4 +1,4 @@
-export const RANDOM_LESSONS = "/api/panel/random-lessons/3";
+export const RANDOM_LESSONS = "/api/panel/random-lessons/";
 export const CATEGORIES = "/api/main-page/categories/Гомель";
 export const CITIES = "/api/common-data/cities";
 export const POPULARS = (city) => `/api/main-page/popular-categories/${city}`;
