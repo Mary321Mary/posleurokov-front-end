8e1a5f2fad7baf997c954239d92e3a5d6aaa89e6
Sheliakhin Mikhail <55269575+GervantFromRibli@users.noreply.github.com>
2022-08-15 17:47:42 +0300

    Add template for loader in catalog

--- posleurokov-app/src/pages/Catalogue/Catalogue.jsx
+++ posleurokov-app/src/pages/Catalogue/Catalogue.jsx
@@ -1,28 +1,44 @@
 import {
   Heading,
   Sheet,
-  VkBlock
+  VkBlock,
+  Loader
 } from 'components';
 import Helmet from 'react-helmet';
+import { useState, useEffect } from 'react';
 
 import styles from './Catalogue.module.scss';
 
 const Catalogue = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+
+    // Вставить то, что рабоает во время загрузки
+
+    setLoading(false);
+  }, [])
+
   return (
     <section className={styles.container}>
-      <Helmet title="Каталог" />
-      <Heading tag='h1'>
-        Каталог кружков, секций и курсов в Гомеле
-      </Heading>
-      <div className={styles['section-list']}>
-        <div className={styles['section-categories']}>
-          <Sheet>
-          </Sheet>
+      {loading ? <Loader /> :
+        <div>
+          <Helmet title="Каталог" />
+          <Heading tag='h1'>
+            Каталог кружков, секций и курсов в Гомеле
+          </Heading>
+          <div className={styles['section-list']}>
+            <div className={styles['section-categories']}>
+              <Sheet>
+              </Sheet>
+            </div>
+            <div className={styles['section-categories']}>
+              <VkBlock heigth='auto' width='220px' />
+            </div>
+          </div>
         </div>
-        <div className={styles['section-categories']}>
-          <VkBlock heigth='auto' width='220px' />
-        </div>
-      </div>
+      }
     </section>
   );
 };
