bcc1016e4591a9dbdfa751e229b4d6f482f8ebfc
Maria Zaitsava <mzaitsava@gomel.iba.by>
2022-08-16 14:35:15 +0300

    change images

--- posleurokov-app/src/components/Additional/Additional.jsx
+++ posleurokov-app/src/components/Additional/Additional.jsx
@@ -2,6 +2,7 @@ import { Link } from "components";
 import styles from "./Additional.module.scss";
 import { useState, useEffect } from "react";
 import { axiosAPI } from "plugins/axios";
+import bgr from "assets/img/bgr.svg";
 
 const Additional = ({ price, ...rest }) => {
   const [additional, setAdditional] = useState([]);
@@ -16,22 +17,62 @@ const Additional = ({ price, ...rest }) => {
   };
 
   return (
-    <Link path="/">
-      <div
-        className={`${styles.additional} ${
-          price ? styles["price"] : styles["no-price"]
-        }`}
-        style={{ ...rest }}
-      >
-        {price ? (
-          additional.price !== null ? (
-            <p className={styles.exist}>{additional.price} Р.</p>
+    <Link path="/" style={{ ...rest }}>
+      {additional.image !== undefined && additional.image.image !== null ? (
+        <div
+          className={`${styles.additional} `}
+          style={{
+            background: `
+              url(${bgr}) bottom no-repeat,
+              url(${
+                process.env.REACT_APP_BASE_URL + additional.image.image
+              }) no-repeat`,
+            backgroundSize: "auto, 230px 250px",
+          }}
+        >
+          {price ? (
+            <div className={styles.union}>
+              {additional.price !== null ? (
+                <p className={styles.exist}>{additional.price} Р.</p>
+              ) : (
+                <p className={styles.absent}>не указана</p>
+              )}
+            </div>
           ) : (
-            <p className={styles.absent}>не указана</p>
-          )
-        ) : null}
-        <p className={styles.name}>{additional.name}</p>
-      </div>
+            <div
+              style={{
+                width: "100px",
+                height: "65px",
+                marginLeft: "146px",
+              }}
+            ></div>
+          )}
+          <p className={styles.name}>{additional.name}</p>
+        </div>
+      ) : (
+        <div
+          className={`${styles.additional} ${
+            price ? styles["price"] : styles["no-price"]
+          }`}
+        >
+          {price ? (
+            additional.price !== null ? (
+              <p className={styles.exist}>{additional.price} Р.</p>
+            ) : (
+              <p className={styles.absent}>не указана</p>
+            )
+          ) : (
+            <div
+              style={{
+                width: "100px",
+                height: "0px",
+                marginLeft: "146px",
+              }}
+            ></div>
+          )}
+          <p className={styles.name}>{additional.name}</p>
+        </div>
+      )}
     </Link>
   );
 };
