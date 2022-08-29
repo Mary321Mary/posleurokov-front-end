21f08dd7f22789e787368b64bd557dc5d00c7cd8
Sheliakhin Mikhail <55269575+GervantFromRibli@users.noreply.github.com>
2022-08-29 21:55:27 +0300

    Use package to change placement

--- posleurokov-app/src/components/shared/Categories/Categories.jsx
+++ posleurokov-app/src/components/shared/Categories/Categories.jsx
@@ -1,12 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./Categories.module.scss";
+import Masonry from 'react-masonry-css';
 
 const Categories = ({ number, children, ...rest }) => {
+  const [columnCount, setColumnCount] = useState(3);
+
+  const getCount = () => {
+    let innerWidth = window.outerWidth;
+
+    if (innerWidth > 1024) {
+      setColumnCount(3)
+    } else if (innerWidth > 700 && innerWidth <= 1024) {
+      setColumnCount(2)
+    } else {
+      setColumnCount(1)
+    }
+  };
+
+  useEffect(() => {
+    getCount();
+
+    function handleWindowResize() {
+      getCount();
+    }
+
+    window.addEventListener("resize", handleWindowResize);
+
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+
   return (
     <div style={{ ...rest }} className={styles.categories}>
       <div className={styles.label}>ВСЕ ЗАНЯТИЯ</div>
       <div className={styles.number}>{number}</div>
-      <div className={styles.content}>{children}</div>
+      <Masonry
+        breakpointCols={columnCount}
+        className={styles.masonry_grid}
+        columnClassName={styles.masonry_grid_column}>
+        {children}
+      </Masonry>
     </div>
   );
 };
