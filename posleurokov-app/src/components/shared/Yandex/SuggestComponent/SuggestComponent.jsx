23b962672de013c2c974cdfed00278934a96d3dd
Sheliakhin Mikhail <55269575+GervantFromRibli@users.noreply.github.com>
2022-09-05 09:40:40 +0300

    Update SuggestComponent.jsx

--- posleurokov-app/src/components/shared/Yandex/SuggestComponent/SuggestComponent.jsx
+++ posleurokov-app/src/components/shared/Yandex/SuggestComponent/SuggestComponent.jsx
@@ -2,7 +2,7 @@ import { useEffect, useMemo } from "react";
 import { YMaps, withYMaps } from "react-yandex-maps";
 import { useSelector } from "react-redux";
 
-const SuggestComponent = ({ handler, value, placeholder, prepend, className, suggestWidth, id = 'suggest', isCitySet = false, ...rest }) => {
+const SuggestComponent = ({ handler, value, placeholder, prepend, className, suggestWidth, suggestId = 'suggest', isCitySet = false, ...rest }) => {
 
   function MapSuggestComponent(props) {
     const { ymaps } = props;
@@ -10,7 +10,7 @@ const SuggestComponent = ({ handler, value, placeholder, prepend, className, sug
 
     useEffect(() => {
       ymaps.ready(() => {
-        const suggestView = new ymaps.SuggestView(id, {
+        const suggestView = new ymaps.SuggestView(suggestId, {
           provider: {
             suggest: request => {
               let boundResult = 'Беларусь, '
@@ -55,7 +55,7 @@ const SuggestComponent = ({ handler, value, placeholder, prepend, className, sug
         type={"text"}
         maxLength={200}
         width={suggestWidth}
-        id={id}
+        id={suggestId}
         defaultValue={value}
         placeholder={placeholder}
         style={{ ...rest }}
