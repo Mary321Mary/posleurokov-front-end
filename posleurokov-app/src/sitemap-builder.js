8e7acc94c63105fb630d8ece95aa83208cfef048
Sheliakhin Mikhail <55269575+GervantFromRibli@users.noreply.github.com>
2022-09-13 13:43:28 +0300

    Update sitemap-builder.js

--- posleurokov-app/src/sitemap-builder.js
+++ posleurokov-app/src/sitemap-builder.js
@@ -15,7 +15,7 @@ async function generateSitemap() {
     let categoryParam = [];
 
     for (let i = 0; i < categories.length; i++) {
-      categoryParam.push(String(categories[i].baseCategory.name).replaceAll(/\s/g, "%20"));
+      categoryParam.push(String(categories[i].baseCategory.name).replace(/\s/g, "%20"));
       for (let j = 0; j < categories[i].concreteCategories.length; j++) {
         categoryParam.push(String(categories[i].concreteCategories[j].name).replace(/\s/g, "%20"));
       }
